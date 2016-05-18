3a8ed96555f0212f1f4c1500ab460468de57582b
wakaru44 <wakaru44@gmail.com>
2016-05-18 16:57:19 +0100

    a simple version with a popup

--- www/launch.js
+++ www/launch.js
@@ -22,3 +22,30 @@ function spinup() {
         'Done'              // buttonName
     );
 }
+
+
+fire_url = "http://m.elo.com.es/v1/fire?s=secret";
+
+call_fire = function (){
+    console.log("Calling the fire at " + fire_url);
+    $.ajax({
+        url:fire_url,
+        type:"GET",
+        dataType:"json",
+        success: onFireSuccess,
+        error: onFireError
+    });
+    console.log("Showing off async");
+};
+
+onFireSuccess = function (data) {
+    // the callback after call_fire
+    // TODO: add a nice succes message
+    console.log("Success")
+};
+
+onFireError = function (data, textStatus, errorThrown) {
+    console.log("data: " + data);
+    console.log("text status: " + textStatus);
+    console.log("Error thrown: " + errorThrown);
+};
